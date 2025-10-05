258eebf2bd76f9ce5ee844ff6edd3fdb0189a495
thomas-bressel <tbressel@outlook.fr>
2025-10-05 16:10:48 +0200

    💄 Create button component with animate hover effect

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@
 
 import style from './Header.module.scss'
 import Button from '@/components/ui/button/Button'
+import {icons} from '@/components/icons/icons'
 
 const Header = () => {
 
@@ -10,13 +11,16 @@ const Header = () => {
         <div className={style.header}>
 
             <div className={style.header__bloc_left}>
-            <img className={style.header__logo} src="/icons/icon-64.png" alt="" />
+                <img className={style.header__logo} src="/icons/icon-64.png" alt="" />
             </div>
-            <div>
-            <Button></Button>
+            <div className={style.header__bloc_middle}>
+                <Button icon={icons.arrowsSpinner} text="refresh" viewBox="0 0 256 256"/>
+                <Button icon={icons.settingWheel} viewBox="0 0 16 16"/>
             </div>
         </div>
     );
 }
 
 export default Header;
+
+
--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,13 +1,16 @@
 import style from './Button.module.scss'
 
 
-const Button = () => {
+const Button = (props: {text?: string, icon?: React.ReactNode, viewBox?: string}) => {
     return (
         <>
             <button className={style.button}>
-                <img className={style.button__icon} src="svg/green/arrow-spinner.svg" alt="" />
-                <span className={style.button__text}>refresh</span>
-
+                {props.icon ?
+                    <svg className={style.button__icon} xmlns="http://www.w3.org/2000/svg" viewBox={props.viewBox}>{props.icon}</svg>
+                : null}
+                
+                
+                {props.text ? <span className={style.button__text}>{props.text}</span> : null}
             </button>
 
 
